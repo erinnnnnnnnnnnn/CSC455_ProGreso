2cb940b1e9b3c0078ef09fbf366fa9187febe6a0
erinnnnnnnnnnnn <151948665+erinnnnnnnnnnnn@users.noreply.github.com>
2025-04-01 20:51:33 +0600

    Update db_connect.php
    nothingg

--- db_connect.php
+++ db_connect.php
@@ -9,5 +9,5 @@ $conn = new mysqli($host, $user, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Database connected successfully!";
+echo "Connected.";
 ?>
