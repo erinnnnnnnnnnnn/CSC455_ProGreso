8ef1f0b4674b3f2fc5115e6f4337817274df4b0f
erinnnnnnnnnnnn <151948665+erinnnnnnnnnnnn@users.noreply.github.com>
2025-04-01 21:02:17 +0600

    database additional

---
+++ database.php
@@ -0,0 +1,20 @@
+<?php
+$host = "localhost"; 
+$user = "root";
+$password = ""; 
+$database = "progreso";
+
+$conn = new mysqli($host, $user, $password);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+if (!$conn->select_db($database)) {
+    die("Database selection failed: " . $conn->error);
+}
+
+echo "Connected to: " . $database;
+?> 
