ef6b424a1ffa7516f451e95478bd4efa401c3bef
erinnnnnnnnnnnn <151948665+erinnnnnnnnnnnn@users.noreply.github.com>
2025-04-29 13:43:55 +0600

    logout updated.

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-session_unset();           // clear session variables
-session_destroy();         // destroy session
-header("Location: landingpage.html"); 
+session_unset();
+session_destroy();
+header("Location: landingpage.html?logout=1");
 exit();
 ?>
